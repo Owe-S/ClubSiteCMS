5908f6aea8ad2db4c0b45295c0c04fb7579b3ab9
Owe Stangeland <128225762+Owe-S@users.noreply.github.com>
2025-06-12 09:23:58 +0200

    Update index.js
    Funksjon: Importer og monter galleryRoutes i index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ import express from 'express';
 // Importerer rute-filene
 import newsRoutes from './routes/news.js';
 import siteSettingsRoutes from './routes/siteSettings.js';
-import userRoutes from './routes/user.js'; // DENNE LINJEN ER NÅ AKTIVERT
+import userRoutes from './routes/user.js';
+import galleryRoutes from './routes/gallery.js'; // NY LINJE
 
 const app = express();
 app.use(express.json()); // Middleware for å parse JSON-requests
@@ -16,7 +17,9 @@ app.use(express.json()); // Middleware for å parse JSON-requests
 // Monterer (bruker) rutene
 app.use('/api/news', newsRoutes);
 app.use('/api/site/settings', siteSettingsRoutes);
-app.use('/api/user', userRoutes); // DENNE LINJEN ER NÅ AKTIVERT
+app.use('/api/user', userRoutes);
+app.use('/api/gallery', galleryRoutes); // NY LINJE
+// app.use('/api/event', eventRoutes); // Denne venter vi med
 
 const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
